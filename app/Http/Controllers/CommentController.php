329c94340f6566df3dfbe037147b3552e0b5dfa4
Basson Thiart <bassonthiart123@gmail.com>
2024-11-28 17:11:08 +0200

    first commit

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Post;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if (!$request->user()) {
+            return response()->json(['message' => 'You are not logged in']);
+        }
+
+        $comments = Comment::get();
+        return response()->json([
+            'comments' => $comments
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $comment = Comment::create([
+            'content' => $data['content'],
+            'post_id' => $data['post_id'],
+            'user_id' => $request->user()->id
+        ]);
+        return response()->json($comment);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $comment = Comment::find($id);
+        return response()->json($comment);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->all();
+        $comment = Comment::find($id);
+
+        if ($comment->user_id !== $request->user()->id) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $comment->update([
+            'content' => $data['content']
+        ]);
+        return response()->json($comment);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, string $id)
+    {
+        $comment = Post::find($id);
+
+        if ($comment->user_id !== $request->user()->id) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $comment->delete();
+        return response()->json(['message' => 'Comment deleted']);
+    }
+}
